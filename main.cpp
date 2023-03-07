7a4c884f3d25f266f61f8c71c1fc04d596025c75
Mwiinga Kayuni <chabotasparrow5@gmail.com>
2023-03-07 20:16:04 +0200

    implmtd reindeer arrival + changes to elf

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
+// #include <iostream>
+#include <cstdio>
 #include <conio.h>
-#include <stdlib.h>
 #include <thread>
 #include <semaphore.h>
 #include <queue>
@@ -9,18 +9,14 @@
 #include <random>
 #include <time.h>
 
-struct p{
-    int pid;
-    std::string p_name;
-};
-
 sem_t job_q_sem;
 sem_t ready_q_sem;
 sem_t paused_q_sem;
 sem_t elf_sem;
 sem_t elf_count_sem;
-sem_t reindeer_sem;
+sem_t rd_sem;
 sem_t rd_count_sem;
+sem_t rd_shed_sem;
 sem_t message_sem;
 sem_t done_list_sem;
 
@@ -28,17 +24,19 @@ std::queue<std::thread::id> job_q;
 std::queue<std::thread::id> ready_q;
 std::queue<std::thread::id> paused_q;
 std::vector<std::thread::id> done_list;
+std::vector<std::thread::id> rd_shed;
 
+const int NUM_OF_ELVES = 100;
+const int NUM_OF_REINDEER = 9;
 int elf_count;
 int rd_count;
-
-void elf_working(int elf_id){
-    std::cout << "Elf " << elf_id << " is working...\n";
-}
+int day;
+bool gone_for_christmas = false;
+bool rd_are_ready = false;
 
 void elf_process(){
     sem_wait(&message_sem);
-    std::cout << "\tElf " << std::this_thread::get_id() << " Needs help!!\n";
+    printf("\tElf %d Needs help!!\n", std::this_thread::get_id());
     sem_post(&message_sem);
     
     int size = 0;
@@ -49,10 +47,10 @@ void elf_process(){
     size = job_q.size();
     sem_post(&job_q_sem);
     
-    sem_wait(&elf_sem);
+    sem_wait(&elf_sem); // allows only 3 elves at a time
 
     sem_wait(&message_sem);
-    std::cout << "\tElf " << std::this_thread::get_id() << " is waiting for others\n";
+    printf("\tElf %d is waiting for others\n", std::this_thread::get_id());
     sem_post(&message_sem);
 
     while(elf_count < 3) {
@@ -67,8 +65,11 @@ void elf_process(){
     ready_q.push(std::this_thread::get_id());
     sem_post(&ready_q_sem);
 
+    //if santa is one for christmas, let other threads execute
+    while(gone_for_christmas) std::this_thread::yield();
+
     sem_wait(&message_sem);
-    std::cout << "\tElf " << std::this_thread::get_id() << " is getting help from Santa\n";
+    printf("\tElf %d is getting help from Santa\n", std::this_thread::get_id());
     sem_post(&message_sem);
 
     sem_post(&elf_sem);
@@ -87,45 +88,83 @@ void elf_process(){
 }
 
 void reindeer_process(){
-    sem_wait(&message_sem);
-    std::cout << "\tReindeer " << std::this_thread::get_id() << " Has come back!!\n";
-    sem_post(&message_sem);
-
-    int rq_size = 0;
-    sem_wait(&reindeer_sem);
-    std::cout << "\tReindeer " << std::this_thread::get_id() << " is Waiting in the stall...\n";
 
     sem_wait(&rd_count_sem);
     rd_count++;
-    
     sem_post(&rd_count_sem);
 
-    //if not all reindeers are back, let other processes run
-    while(rd_count < 9){
-        std::this_thread::yield();
-    }
+    sem_wait(&message_sem);
+    printf("\tReindeer %d is back and Waiting in the stall. Reindeer count %d\n", 
+        std::this_thread::get_id(), rd_count);
+    sem_post(&message_sem);
 
-    sem_wait(&ready_q_sem);
-    rq_size = ready_q.size();
-    sem_post(&ready_q_sem);
+    int shed_size = 0;
 
-    while(rq_size < 3) std::this_thread::yield();
+    sem_wait(&rd_shed_sem);
+    rd_shed.push_back(std::this_thread::get_id());
+    shed_size = rd_shed.size();
+    sem_post(&rd_shed_sem);
 
-    sem_wait(&ready_q_sem);
-    ready_q.push(std::this_thread::get_id());
-    sem_push(&ready_q_sem);
+    //if not all reindeers are back, 
+    while(!gone_for_christmas) std::this_thread::yield();
 
-    sem_post(&reindeer_sem);
+    sem_wait(&message_sem);
+    printf("\tReindeer %d is gettin hitched!\n", std::this_thread::get_id());
+    sem_post(&message_sem);
+
+    sem_wait(&rd_shed_sem);
+    rd_shed.pop_back();
+    sem_post(&rd_shed_sem);
+
+    sem_wait(&rd_count_sem);
+    rd_count--;
+    sem_post(&rd_count_sem);
 }
 
+void reindeers(){    
+    std::thread reindeer_ths[NUM_OF_REINDEER];
+    int urgency_factor = 0;
+    sem_init(&message_sem, 0, 1);
 
-int main(){
-      
-    int NUM_OF_ELVES = 100;
-    int NUM_OF_REINDEER = 9;
-    elf_count = 0;
-    rd_count = 0;
+    int i = 0;
+    int rand_val;
 
+    sem_wait(&message_sem);
+    printf("\tIts December, the Reindeers start coming back!\n");
+    sem_post(&message_sem);
+
+    //days to christmas
+    while(day < 357 && !gone_for_christmas){  
+        urgency_factor = (357 - day) * 1.2;
+
+        rand_val = rand() % 100;
+
+        //let reindeer come back at random
+        if(rand_val < 100 - urgency_factor && i < NUM_OF_REINDEER){
+            reindeer_ths[i] = std::thread(&reindeer_process);
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        }
+
+        //when there are nine reindeer, end loop
+        if(rd_count == 9) {
+            sem_wait(&message_sem);
+            printf("\tAll 9 reindeer are back!\n");
+            sem_post(&message_sem);
+            break;
+        }
+        i++;
+    }
+
+    while(rd_shed.size() != 0) std::this_thread::yield();
+
+    rd_are_ready = true;
+
+    for(int i = 0; i < NUM_OF_REINDEER; i++){
+        reindeer_ths[i].join();
+    }
+}
+
+void init_semaphores(){
     sem_init(&job_q_sem, 0, 1);
     sem_init(&ready_q_sem, 0, 1);
     sem_init(&paused_q_sem, 0, 1);
@@ -134,48 +173,43 @@ int main(){
     sem_init(&message_sem, 0, 1);
     sem_init(&done_list_sem, 0, 1);
     sem_init(&elf_sem, 0, 3);
+    sem_init(&rd_sem, 0, 9);
+}
 
-    std::cout << "Main thread: " << std::this_thread::get_id() << "\n";
-
+void north_pole(){
     std::thread elf_ths[NUM_OF_ELVES];
-    std::thread reindeer_ths[NUM_OF_REINDEER];
+    std::thread reindeers_th;
 
-    int day = 0;
-
-    int elf_index = 0;
-    int rd_index = 0;
+    int elf_i = 0;
 
     srand(time(0));
     do{
-        std::cout << "Day: " << day << "\n";
+        sem_wait(&message_sem);
+        printf("Day: %d\n", day);
+        sem_post(&message_sem);
 
-        if(day > 365) day = 0;
+        if(day == 365 - 31){
+            reindeers_th = std::thread(&reindeers);         
+        }
 
-        if(day > 365 - 31){
-            if(day == 365 - 30){
-                sem_wait(&message_sem);
-                std::cout << "Its December, the Reindeers start coming back!\n";
-                sem_post(&message_sem);
-            }
-            
-            if(rand() % 100 < day - 290 && rd_index < NUM_OF_REINDEER){
-                reindeer_ths[rd_index] = std::thread(&reindeer_process);
-                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-                rd_index++;
-            }
+        if(day == 354){
+            sem_wait(&message_sem);
+            printf("\nSanta is ready for christmas!!!\n");
+            sem_post(&message_sem);
 
-            if(day == 364){
-                for(int i = 0; i < NUM_OF_REINDEER; i++){
-                    reindeer_ths[i].join();
-                }
-            }
+            gone_for_christmas = true;
+        }
+
+        if(day > 365) {
+            day = 0;
+            reindeers_th.join();
         }
 
         // elves randomly need help
-        if(rand() % 100 < 20 && elf_index < NUM_OF_ELVES){
-            elf_ths[elf_index] = std::thread(&elf_process);
+        if(rand() % 100 < 20 && elf_i < NUM_OF_ELVES){
+            elf_ths[elf_i] = std::thread(&elf_process);
             std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-            elf_index++;
+            elf_i++;
         }
         
         // if elf process is done executing, join the thread
@@ -183,7 +217,7 @@ int main(){
             for(int k = 0; k < NUM_OF_ELVES; k++){
                 if(elf_ths[k].get_id() == done_list.at(j)){
                     sem_wait(&message_sem);
-                    std::cout << "\tElf " << done_list.at(j) << " is done\n";
+                    printf("\tElf %d is done\n", done_list.at(j));
                     sem_post(&message_sem);
                     elf_ths[k].join();
                     break;
@@ -193,7 +227,18 @@ int main(){
 
         day++;
     }while(day < 1000);
+}
 
+int main(){
+    elf_count = 0;
+    rd_count = 0;
+    day = 0;
+
+    init_semaphores();
+
+    printf("Main thread: %d\n", std::this_thread::get_id());
+
+    north_pole();
 
     return 0;
 }
